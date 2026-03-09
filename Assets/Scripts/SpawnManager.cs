5482c494ef406f3dbecd0cb71ecc949c946c4086
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-03-09 14:44:33 +0000

    Lesson 2.3 - Passo 3
    Spawn random animals from array
    
    João Tomas Ribeiro a63580

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //Variaveis
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    //public int animalIndex;
     void Update()
     {
       if(Input.GetKeyDown(KeyCode.S))
         {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0,0, 20), 
             animalPrefabs[animalIndex].transform.rotation);
         }  
