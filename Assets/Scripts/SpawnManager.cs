21ba488e0cb2a8cbe07cdcc8410441447cf6c8e7
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-03-09 14:59:30 +0000

    Lesson 2.3 - Random Animal Stampede
    Joao Tomas Ribeiro a63580

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,16 +1,23 @@
+
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    //Variaveis
+//Variaveis
     public GameObject[] animalPrefabs;
     //public int animalIndex;
+    //Posição dos animais:
+    private int spawnRangeX = 20;
+    private int spawnPosZ = 20;
     void Update()
     {
       if(Input.GetKeyDown(KeyCode.S))
         {
+            //Gera o animal numa posicao aleatoria:
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);  
+            //Gera um animal aleatorio:
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0, 20), 
+            Instantiate(animalPrefabs[animalIndex], spawnPos, 
             animalPrefabs[animalIndex].transform.rotation);
         }  
     }
