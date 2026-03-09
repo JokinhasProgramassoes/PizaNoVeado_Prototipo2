7fbc47a8c64ea4dac6fe0480ea2c41f0709a66c6
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-03-09 15:19:24 +0000

    Lesson 2.4 - Collision Decisions
    Joao Tomas Ribeiro a63580

--- Assets/Scripts/Destroy out of bounds script.cs
+++ Assets/Scripts/Destroy out of bounds script.cs
@@ -5,13 +5,12 @@ public class Destroyoutofboundsscript : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound) 
@@ -20,6 +19,7 @@ public class Destroyoutofboundsscript : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,24 +1,38 @@
 
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SpawnManager : MonoBehaviour
 {
 //Variaveis
     public GameObject[] animalPrefabs;
-    //public int animalIndex;
     //Posição dos animais:
     private int spawnRangeX = 20;
     private int spawnPosZ = 20;
+    //Invoke Reapiting:
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
     void Update()
     {
-      if(Input.GetKeyDown(KeyCode.S))
-        {
-            //Gera o animal numa posicao aleatoria:
+      //if(Input.GetKeyDown(KeyCode.S))
+        //{
+          // SpawnRandomAnimal();
+        //}  
+    }
+
+    void SpawnRandomAnimal ()
+    {
+         //Gera o animal numa posicao aleatoria:
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);  
             //Gera um animal aleatorio:
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos, 
             animalPrefabs[animalIndex].transform.rotation);
-        }  
     }
 }
